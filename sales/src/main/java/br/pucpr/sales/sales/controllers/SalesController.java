8fb3e957f51dba2bcc46e5bcf10c0cdffaca20f0
Joao Guis <joao.guilherme@eurekalabs.com.br>
2022-11-11 17:31:45 -0300

    add sales today report endpoint

--- sales/src/main/java/br/pucpr/sales/sales/controllers/SalesController.java
+++ sales/src/main/java/br/pucpr/sales/sales/controllers/SalesController.java
@@ -1,7 +1,11 @@
 package br.pucpr.sales.sales.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -29,6 +33,16 @@ public class SalesController {
         Sale sale = this.salesService.save(CreateSaleDTO.toEntity(createSaleDTO));
 
         return this.saleTranslator.toDTO(sale);
+    }
+
+    @GetMapping("/today-report")
+    List<GetSaleDTO> todayReport() {
+        List<Sale> sales = this.salesService.list();
+
+        List<GetSaleDTO> getSaleDTOs = new ArrayList<GetSaleDTO>();
+        sales.forEach(sale -> getSaleDTOs.add(this.saleTranslator.toDTO(sale)));
+
+        return getSaleDTOs;
     }
 
 }
--- sales/src/main/java/br/pucpr/sales/sales/service/SalesService.java
+++ sales/src/main/java/br/pucpr/sales/sales/service/SalesService.java
@@ -1,5 +1,7 @@
 package br.pucpr.sales.sales.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import br.pucpr.sales.sales.entities.Sale;
@@ -12,5 +14,9 @@ public class SalesService {
 
     public Sale save(Sale customer) {
         return this.salesRepository.save(customer);
+    }
+
+    public List<Sale> list() {
+        return (List<Sale>) this.salesRepository.findAll();
     }
 }
